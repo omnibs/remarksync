9956c553944d590600f926dd67a340c2e43dea24
Juliano Bortolozzo Solanho <juliano.solanho@gmail.com>
2016-05-22 18:45:00 -0300

    Heroku dployment config

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule Remarksync.UserSocket do
   channel "slideshow:*", Remarksync.SlideshowChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
